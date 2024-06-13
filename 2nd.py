26816c36b94357579f98f29b9465de1d474cf4b9
PunitVerma0009 <punitverma0009@gmail.com>
2024-06-13 18:26:26 +0530

    more efficient stone paper scissor

--- 2nd.py
+++ 2nd.py
@@ -1,3 +1,34 @@
-print("testing git repository")
-print("testing git repository")
-print("testing git repository")
+import random
+l=['Stone','Paper','Scissor']
+result=[['Draw','Lose','Win'],['Win','Draw','Lose'],['Lose','Win','Draw']]
+print(" Lets play Stone,Paper and Scissor-> \n Remember if you want to exit just press e")
+me=0
+com=0
+dr=0
+while(True):
+    print(" 0 for Stone \n 1 for Paper \n 2 for scissor")
+    userInput=input("Enter your choice=")
+    if userInput.capitalize()=="E":
+        print(f'total scores= \n you={me} \n computer={com} \n draw={dr}')
+        if me>com:
+            print("    Overall you win     ")
+        elif me<com:
+             print("    Overall you lose     ")   
+        else:
+            print("    Overall draw     ")      
+        print("exiting game.....")
+        break
+    elif 0<=int(userInput)<3 :
+        computerOut=random.randint(0,2)
+        print("computer output=",l[computerOut])
+        if result[int(userInput)][computerOut]=="Draw":
+            print("      Draw      ")
+            dr=dr+1
+        elif result[int(userInput)][computerOut]=="Win":
+            print("     You Win     ")
+            me=me+1
+        else:
+            print("     You Lose     ")
+            com+=1  
+    else:
+        print("invalid input")         
